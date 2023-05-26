1ddf646c7738dc2dabcd138d64362e20e5401a8b
giorgio <giorgiosavron@me.com>
2023-05-26 13:15:21 -0400

    links

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,22 +1,40 @@
 import React, { Component } from "react";
 import "./footer.css";
+import gs from "../images/gs_logo.png";
+import linkedin from "../images/linkedin.png";
+import github from "../images/github.png";
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
         <ul className="list-inline text-center">
-          <li className="list-inline-item li-description">
-            <i className="fab fa-instagram"></i>
-          </li>
-          <li className="list-inline-item li-description">
-            <i className="fab fa-facebook-f"></i>
-          </li>
-          <li className="list-inline-item li-description">
-            <i className="fab fa-twitter"></i>
-          </li>
+          <a
+            className="text-muted list-inline-item li-description mx-2"
+            href="https://www.giorgiosavron.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={gs} alt="GS-Logo" eight={25} width={25} />
+          </a>
+          <a
+            className="text-muted list-inline-item li-description mx-2"
+            href="https://www.linkedin.com/in/giorgio-savron/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} alt="Linkedin-Logo" height={25} width={25} />
+          </a>
+          <a
+            className="text-muted list-inline-item li-description mx-2"
+            href="https://github.com/gsavr/owlz-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} alt="github-Logo" eight={25} width={25} />
+          </a>
         </ul>
-        <div className="footer-copyright text-center ">
+        <div className="footer-copyright text-center">
           © 2019 Giorgio Savron & Florian Lahitte
         </div>
       </div>
